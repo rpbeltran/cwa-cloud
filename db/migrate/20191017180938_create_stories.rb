560ec53e4f77e5828b2d06ffbea5f31f6ab25f30
Glen Lauritsen <GDL2021@tamu.edu>
2019-10-18 23:21:05 +0000

    updated migrate for the story model, removed comments in seeds

--- db/migrate/20191017180938_create_stories.rb
+++ db/migrate/20191017180938_create_stories.rb
@@ -1,7 +1,10 @@
 class CreateStories < ActiveRecord::Migration[6.0]
   def change
     create_table :stories do |t|
-
+      t.string :author
+      t.string :storyName
+      
+      
       t.timestamps
     end
   end
