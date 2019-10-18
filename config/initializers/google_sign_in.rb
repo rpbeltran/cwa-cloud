f4586bf0c8b196b61ef32ef4601c47786d115e91
= <=>
2019-10-18 07:11:39 +0000

    Works in C9, still not in heroku

--- config/initializers/google_sign_in.rb
+++ config/initializers/google_sign_in.rb
@@ -1,6 +1,6 @@
 
 
 Rails.application.configure do
-  config.google_sign_in.client_id     = ENV['google_sign_in_client_id']
-  config.google_sign_in.client_secret = ENV['google_sign_in_client_secret']
+  config.google_sign_in.client_id     = ENV['GOOGLE_CLIENT_ID']
+  config.google_sign_in.client_secret = ENV['GOOGLE_SECRET']
 end
