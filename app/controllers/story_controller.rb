65f42af745c7ed34fadd411e2bdd7713f1cd1740
Glen Lauritsen <GDL2021@tamu.edu>
2019-10-30 20:39:33 +0000

    finished reworking parameter validation

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -15,13 +15,30 @@ class StoryController < ApplicationController
     def create
         puts 'Entered the Create Method'
         
-        begin
-            @story = Story.create!(story_params)
-        rescue => error
-            # refine this to all for better feedback on the missing / wrong params
-            puts 'Missing One or More Required Params'
-            puts error
+        # logic to help debug form submit error
+        # convert these into notices eventually
+        
+        if params[:title] == "" then
+           puts 'Missing Require Field: Title'
+           redirect_to story_new_path
+           return
+        elsif params[:firstname] == "" then
+           puts 'Missing Require Field: First Name'
+           redirect_to story_new_path
+           return
+        elsif params[:lastname] == "" then
+           puts 'Missing Require Field: Last Name'
+           redirect_to story_new_path
+           return
+        elsif params[:file] == "" then
+           puts 'Missing Require Field: File'
+           redirect_to story_new_path
+           return
         end
+            
+        @story = Story.create(story_params)
+        puts 'Successfully Made New Story'
+        
         # go back to the new view
         redirect_to story_new_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  resources :story
   get 'story/new', to: 'story#new', as: :story_new
-  get 'story/create', to: 'story#create', as: :story_create
+  post 'story/create', to: 'story#create', as: :story_create
   root 'story#new'
   
   get 'login', to: 'logins#receive'
