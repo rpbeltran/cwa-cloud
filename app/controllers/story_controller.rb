32abb268be7ea307275d4d3e040bd0d3754d247b
Glen Lauritsen <GDL2021@tamu.edu>
2019-10-31 14:00:31 +0000

    revised plan for submitting tags

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -15,29 +15,36 @@ class StoryController < ApplicationController
     def create
         puts 'Entered the Create Method'
         
+        if params[:commit] == "Add Tag" then
+            puts "Adding a Tag"
+        elsif params[:commit] == "Submit Story" then
+            puts "Submitting a Story"
+        end
+        
         # logic to help debug form submit error
         # convert these into notices eventually
         
-        if params[:title].to_s.strip.empty? then
-           puts 'Missing Require Field: Title'
-           redirect_to story_new_path
-           return
-        elsif params[:firstname].to_s.strip.empty? then
-           puts 'Missing Require Field: First Name'
-           redirect_to story_new_path
-           return
-        elsif params[:lastname].to_s.strip.empty? then
-           puts 'Missing Require Field: Last Name'
-           redirect_to story_new_path
-           return
-        elsif params[:file].to_s.strip.empty? then
-           puts 'Missing Require Field: File'
-           redirect_to story_new_path
-           return
-        end
-            
-        @story = Story.create(story_params)
-        puts 'Successfully Made New Story'
+        # todo: optomize logic here
+        #if params[:title].to_s.strip.empty? then
+        #   puts 'Missing Require Field: Title'
+        #   redirect_to story_new_path
+        #   return
+        #elsif params[:firstname].to_s.strip.empty? then
+        #   puts 'Missing Require Field: First Name'
+        #   redirect_to story_new_path
+        #   return
+        #elsif params[:lastname].to_s.strip.empty? then
+        #   puts 'Missing Require Field: Last Name'
+        #   redirect_to story_new_path
+        #   return
+        #elsif params[:file].to_s.strip.empty? then
+        #   puts 'Missing Require Field: File'
+        #   redirect_to story_new_path
+        #   return
+        #end
+        #    
+        #@story = Story.create(story_params)
+        #puts 'Successfully Made New Story'
         
         # go back to the new view
         redirect_to story_new_path
