8fd56f358d245b2b09e426acbb688615a5405703
Emily O'Dea <emilyodea1@gmail.com>
2019-11-01 22:35:15 +0000

    rpsec tests for upload stories

--- db/migrate/20191017180938_create_stories.rb
+++ db/migrate/20191017180938_create_stories.rb
@@ -1,4 +1,5 @@
-class CreateStories < ActiveRecord::Migration[6.0]
+class CreateStories < ActiveRecord::Migration[6.0] 
+  
   def change
     create_table :stories do |t|
       t.string :title
--- spec/logins_spec.rb
+++ spec/logins_spec.rb
@@ -1,32 +1,32 @@
-require_relative './rails_helper.rb'
-require_relative '../app/controllers/logins_controller'
+# require_relative './rails_helper.rb'
+# require_relative '../app/controllers/logins_controller'
 
-#Dir["../app/controllers/*.rb"].each {|file| require file }
+# #Dir["../app/controllers/*.rb"].each {|file| require file }
 
-#describe 'LoginsController' do
-#the following is an example test that I've been trying to get working
-RSpec.describe LoginsController, :type => :controller do
-    describe '#sum' do 
-      it 'should be defined' do
-        expect { sum([1, 3, 4]) }.not_to raise_error
-      end
+# #describe 'LoginsController' do
+# #the following is an example test that I've been trying to get working
+# RSpec.describe LoginsController, :type => :controller do
+#     describe '#sum' do 
+#       it 'should be defined' do
+#         expect { sum([1, 3, 4]) }.not_to raise_error
+#       end
   
-      it 'returns correct sum [20 points]', points: 20 do
-        expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
-        expect(sum([1, 2, 3, 4, 5])).to eq(15)
-        expect(sum([1, 2, 3, 4, -5])).to eq(5)
-        expect(sum([1, 2, 3, 4, -5, 5, -100])).to eq(-90)
-      end
+#       it 'returns correct sum [20 points]', points: 20 do
+#         expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
+#         expect(sum([1, 2, 3, 4, 5])).to eq(15)
+#         expect(sum([1, 2, 3, 4, -5])).to eq(5)
+#         expect(sum([1, 2, 3, 4, -5, 5, -100])).to eq(-90)
+#       end
   
-      it 'works on the empty array [10 points]', points: 10 do
-        expect { sum([]) }.not_to raise_error
-        expect(sum([])).to be_zero
-      end
-    end
-   # describe '#receive' do
-   #   it 'should be defined' do
-   #     #expect { receive(test) }.not_to raise_error
-   #     expect(receive).to respond_to(:receive) 
-   #   end
-   # end
-end
+#       it 'works on the empty array [10 points]', points: 10 do
+#         expect { sum([]) }.not_to raise_error
+#         expect(sum([])).to be_zero
+#       end
+#     end
+#   # describe '#receive' do
+#   #   it 'should be defined' do
+#   #     #expect { receive(test) }.not_to raise_error
+#   #     expect(receive).to respond_to(:receive) 
+#   #   end
+#   # end
+# end
