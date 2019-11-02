9f89deeeb48d8f44b7ba38c6bca8fca0a0139610
= <=>
2019-11-02 01:47:02 +0000

    Admins are live

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -4,12 +4,34 @@ class RulesController < ApplicationController
         params.require(:value)
     end
     
+    def isAdmin
+        if cookies.key?(:user_id)
+            user = User.find_by user_id: cookies.signed[:user_id]
+            return user.admin
+        end
+        return false
+    end    
+    
+    def username
+        if cookies.key?(:user_id)
+            user = User.find_by user_id: cookies.signed[:user_id]
+            return user.name
+        end
+        return false
+    end
+
+    
     def main
         @rules = Rule.all
+        @admin = isAdmin
+        @username = username
     end
     
     def new
-        Rule.create({ :value => params[:value] })
-        redirect_to "/rules/"
+        
+        if isAdmin
+            Rule.create({ :value => params[:value] })
+            redirect_to "/rules/"
+        end
     end
 end
