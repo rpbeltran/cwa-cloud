1f23820d467a43bc925e8c9dbb0c3a9fcb596830
= <=>
2019-11-14 08:28:05 +0000

    refactored loggin info

--- app/controllers/administer_controller.rb
+++ app/controllers/administer_controller.rb
@@ -4,33 +4,18 @@ class AdministerController < ApplicationController
         User.where(:admin => true).count
     end
     
-    def isAdmin
-        if cookies.key?(:user_id)
-            user = User.find_by user_id: cookies.signed[:user_id]
-            return user.admin
-        end
-        return false
-    end    
-    
-    def username
-        if cookies.key?(:user_id)
-            user = User.find_by user_id: cookies.signed[:user_id]
-            return user.name
-        end
-        return false
+    def access
+        return isAdmin || countAdmins == 0
     end
-
+    
     def main
         @users = User.all
-        @admin = isAdmin
-        @username = username
         @admin_count = countAdmins
-        puts @admin_count.class
-        @vis = isAdmin || @admin_count == 0
-    end    
+        @access = access
+    end
     
     def new
-        if isAdmin || countAdmins == 0
+        if access
             User.where(:user_id => params[:sel_id]).update_all(:admin => true)
             redirect_to "/admin"
         end
--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -3,27 +3,9 @@ class RulesController < ApplicationController
     def rule_params
         params.require(:value)
     end
-    
-    def isAdmin
-        if cookies.key?(:user_id)
-            user = User.find_by user_id: cookies.signed[:user_id]
-            return user.admin
-        end
-        return false
-    end    
-    
-    def username
-        if cookies.key?(:user_id)
-            user = User.find_by user_id: cookies.signed[:user_id]
-            return user.name
-        end
-        return false
-    end
 
     def main
         @rules = Rule.all
-        @admin = isAdmin
-        @username = username
     end
     
     def new
