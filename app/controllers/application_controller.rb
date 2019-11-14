4f9634287753edad5d610a7af4117ae3bc930689
= <=>
2019-11-14 08:14:26 +0000

    Updated signin button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
+
+
 class ApplicationController < ActionController::Base
+    
+    def isLogged
+        return cookies.key?(:user_id)
+    end
+        
+    def isAdmin
+        if isLogged
+            user = User.find_by user_id: cookies.signed[:user_id]
+            return user.admin
+        end
+        return false
+    end    
+    
+    def username
+        if isLogged
+            user = User.find_by user_id: cookies.signed[:user_id]
+            return user.name
+        end
+        return false
+    end
+    
+    before_action :initialize_logged_user
+    def initialize_logged_user
+        @is_logged    = isLogged
+        @logged_user  = username
+        @logged_admin = isAdmin
+    end
+    
 end
